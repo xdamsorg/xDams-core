daf9e1068b8c515d3e9d7fd132e1729d52bce4d9
xDams.org <support@xdams.org>
2015-03-02 17:24:55 +0100

    removed js ante multilang

--- src/main/webapp/resources/xd-js-original/xdams_help.js
+++
@@ -1,5 +0,0 @@
-/* controllato da Diego il 01/12/2007 */
-function help(thePage){
-	theMenu = window.open('/dams_common/help/'+thePage+'.html','help','width=520px, height=350px, scrollbars=yes, resizable=yes, location=no');
-	return false;
-}
