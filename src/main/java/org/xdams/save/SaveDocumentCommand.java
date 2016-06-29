73d73b59034a3e36ca7f3ba51726af09c761adc1
xDams.org <support@xdams.org>
2016-06-29 17:33:48 +0200

    nd

--- src/main/java/org/xdams/save/SaveDocumentCommand.java
+++ src/main/java/org/xdams/save/SaveDocumentCommand.java
@@ -1,9 +1,3 @@
-/*
- * Created on 1-giu-2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 package org.xdams.save;
 
 import java.util.Map;
