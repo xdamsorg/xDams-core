10026cc897b977258f0196289f90fc3f8af3895b
xDams.org <support@xdams.org>
2016-10-05 16:19:30 +0200

    fix workspace order

--- src/main/java/org/xdams/controller/xDamsController.java
+++ src/main/java/org/xdams/controller/xDamsController.java
@@ -167,8 +167,8 @@ public class xDamsController {
 
 	@RequestMapping(value = { "/home", "/" }, method = RequestMethod.GET)
 	public String home(Model model, @ModelAttribute("userBean") UserBean userBean) throws Exception {
-		// Map<String, List<Archive>> usersArchives = new LinkedHashMap<String, List<Archive>>();
-		Map<String, List<Archive>> usersArchives = new TreeMap<String, List<Archive>>();
+		Map<String, List<Archive>> usersArchives = new LinkedHashMap<String, List<Archive>>();
+		// Map<String, List<Archive>> usersArchives = new TreeMap<String, List<Archive>>();
 		serviceUser.loadArchives(userBean, usersArchives);
 		model.addAttribute("usersArchives", usersArchives);
 		return "user/workspace";
