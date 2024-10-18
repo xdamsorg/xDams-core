a95d6ff7d07ffedd54b3dfd4824a1726d3423790
simvox <spasquini@regesta.com>
2024-10-18 17:51:46 +0200

    signup autologin

--- src/main/java/org/xdams/controller/xDamsAutoLoginController.java
+++ src/main/java/org/xdams/controller/xDamsAutoLoginController.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +20,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.xdams.security.AuthenticationFilter;
 import org.xdams.security.AuthenticationType;
+import org.xdams.security.UserDetails;
+import org.xdams.security.load.LoadUserManager;
 import org.xdams.user.access.ServiceUser;
+import org.xdams.user.bean.Archive;
+import org.xdams.user.bean.UserBean;
 
 @Controller
 @SessionAttributes({ "userBean" })
@@ -47,11 +52,28 @@ public class xDamsAutoLoginController {
 	AuthenticationFilter formLoginFilter;
 
 	@RequestMapping(value = "/autologin/{account}/{userGuest}/signup", method = RequestMethod.GET)
-	public String createNewUser(@PathVariable String account, @PathVariable String userGuest, HttpServletRequest request, HttpServletResponse response) {
+	public String signup(@PathVariable String account, @PathVariable String userGuest, HttpServletRequest request, HttpServletResponse response) {
 		if (authenticationType.isAllowAutoLogin()) {
 			authenticateUserAndSetSession(account, userGuest, request);
 		}
 		return "redirect:/home";
+	}
+
+	@RequestMapping(value = "/autologin/{account}/{userGuest}/{archive}/signup", method = RequestMethod.GET)
+	public String signupQuery(@PathVariable String account, @PathVariable String userGuest, @PathVariable String archive, HttpServletRequest request, HttpServletResponse response) {
+		if (authenticationType.isAllowAutoLogin()) {
+			authenticateUserAndSetSession(account, userGuest, request);
+		}
+		UserDetails userDetails = null;
+		try {
+			userDetails = (UserDetails) ((SecurityContext) SecurityContextHolder.getContext()).getAuthentication().getPrincipal();
+			UserBean userBean = LoadUserManager.executeLoad(userDetails, authenticationType);
+			Archive a = ServiceUser.getArchive(userBean, archive);
+		} catch (Exception e) {
+			SecurityContextHolder.getContext().setAuthentication(null);
+			return "redirect:/login";
+		}
+		return "redirect:/search/" + archive + "/query.html";
 	}
 
 	private void authenticateUserAndSetSession(String account, String userGuest, HttpServletRequest request) {
